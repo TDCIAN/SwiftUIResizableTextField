f28ac725c11975ad004256c579d1314673b2aec4
TDCIAN <tdcian71@gmail.com>
2021-08-20 10:14:03 +0900

    Initial Commit

---
+++ ResizableTextField/ResizableTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizableTextFieldApp.swift
+//  ResizableTextField
+//
+//  Created by 김정민 on 2021/08/20.
+//
+
+import SwiftUI
+
+@main
+struct ResizableTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
